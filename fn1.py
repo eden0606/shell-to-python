9d3e684613901b1b923ad588e4e97e955ff1edf5
Eden Chou <eden.chou0606@gmail.com>
2020-06-16 11:32:23 -0400

    fn1 updates

---
+++ fn1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# logger function
+
+def logger(dt, f, l, h,m):
+	print("
--- practice.sh
+++ practice.sh
@@ -1,3 +1,16 @@
-!#/bin/sh
+#!/bin/sh
 
-echo "this is a new line
+pipes() {
+	local NAME1=$1
+	local NAME2=$2
+	local NAME3=$3
+	local NAME4=$4
+	echo "${NAME1}|${NAME2}|${NAME3}" >> "${NAME4}"
+}
+
+echo "Names added to file!"
+pipes ${1} ${2} ${3} ${4}
+
+# pipes takes in parameters and redirects echo string "NAME1|NAME2|NAME3" into text file named 
+# the parameter NAME4 if NAME4 does not exist, a new one is created
+# if one already exists, names are appended to the end of the document
