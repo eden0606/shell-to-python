8b5ba21402d2f8bfe54a54146ad21599047c2f4a
Eden Chou <eden.chou0606@gmail.com>
2020-06-29 17:01:06 -0400

    calling shell functions in python

---
+++ callee.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name() {
+	local NAME=$1
+	echo Hi my name is $NAME
+}
+
+
+name
---
+++ caller.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import subprocess, os
+import shlex
+
+print("This is my introduction:")
+
+subprocess.Popen(['./callee.sh', "Eden"])
+
+#subprocess.Popen(['./callee.sh', &name 'Eden'])
+
+#subprocess.call([". \"./callee.sh\";", "&name"])
+
+#subprocess.call(shlex.split('./callee.sh name "Eden"'))
+#subprocess.call(['./callee.sh'])
+#exec(open("./callee.sh").read())
+
+
+
