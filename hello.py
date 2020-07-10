146f285f3ab862b5164f30c412aba5592b1e13fc
Eden Chou <eden.chou0606@gmail.com>
2020-07-10 17:20:16 -0400

    curl stuff

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 
 def Hello (name):
 	print("hello " + name + " welcome to my world")
+	arr=["hi", "bye", "goodbye"]
+	o=open("output", "a")
+	print("\n".join(arr), file=o)
 
 print("calling hello function")
 Hello("Eden")
--- hello.sh
+++ hello.sh
@@ -3,8 +3,17 @@
 function Hello() {
 	local NAME=$1
 	echo "hello $NAME welcome to my world"
-
+	echo ["hi", "bye", "goodbye"] >> output
+	tr -s ' '  '\n' < output > hello
+	
 }
 
-echo "calling hello function"
+#echo "calling hello function"
 Hello Eden
+
+
+
+
+
+
+
